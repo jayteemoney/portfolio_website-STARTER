929229d1cf2e0d34695b694bf2fd73ff3a091a60
jayteemoney <jethroirmiya@gmail.com>
2025-04-17 15:34:54 +0000

    hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,20 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+<Section row nopadding>
+  <LeftSection>
+    <SectionTitle main center>
+      Welcome To <br />
+      My Portfolio
+    </SectionTitle>
+    <SectionText>
+     i am responsible for creating responsive, user-friendly interfaces that connect users with the functionality of a website or application.
+    </SectionText>
+    <Button onClick={()=> window.location="https://google.com"}>Learn more</Button>
+  </LeftSection>
+
+</Section>
 );
 
 export default Hero;
