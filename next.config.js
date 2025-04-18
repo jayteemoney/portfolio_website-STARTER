86ce7c31244f098f7bb062d6041f6a061f89a800
jayteemoney <jethroirmiya@gmail.com>
2025-04-18 17:38:28 +0000

    package

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// /** @type {import('next').NextConfig} **/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-      unoptimized: true, // Required for static exports with next/image
+    compiler: {
+      emotion: true, // Enable Emotion support
     },
   };
   
-  export default nextConfig;
+  module.exports = nextConfig;
