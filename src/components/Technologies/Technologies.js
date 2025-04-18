bd0f8d9761e6084ee8d76733d2078874640a65c3
jayteemoney <jethroirmiya@gmail.com>
2025-04-18 07:39:27 +0000

    tech

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,46 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id ="tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>As a front-end developer, I craft responsive, interactive user interfaces using HTML, CSS, JavaScript, and frameworks like React and Tailwind. I utilize Git, Webpack, and Figma to streamline development, optimize performance, and bring designs to life.
+
+</SectionText>
+<List>
+  <ListItem>
+    <DiReact size="3rem" />
+    <ListContainer>
+      <ListTitle>Front-End</ListTitle>
+      <ListParagraph>
+        Experience with <br />
+        React.js
+      </ListParagraph>
+    </ListContainer>
+  </ListItem>
+  <ListItem>
+    <DiFirebase size="3rem" />
+    <ListContainer>
+      <ListTitle>Blockchain</ListTitle>
+      <ListParagraph>
+        Experience with <br />
+        Solidity Smart contracts
+      </ListParagraph>
+    </ListContainer>
+  </ListItem>
+  <ListItem>
+    <DiReact size="3rem" />
+    <ListContainer>
+      <ListTitle>Front-End</ListTitle>
+      <ListParagraph>
+        Experience with <br />
+        APIs: Fetch or Axios for integrating backend data.
+      </ListParagraph>
+    </ListContainer>
+  </ListItem>
+</List>
+  </Section>
 );
 
 export default Technologies;
